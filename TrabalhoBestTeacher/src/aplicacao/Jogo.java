f81b8b6cd66855c5d722a871057eecef57e78509
agm <agm@guilherme>
2024-11-22 15:53:20 -0300

    teste

--- TrabalhoBestTeacher/src/aplicacao/Jogo.java
+++ TrabalhoBestTeacher/src/aplicacao/Jogo.java
@@ -5,3 +5,14 @@ import entidades.Monstro;
 import entidades.Personagem;
 
 import java.util.Random;
+
+public class Jogo {
+
+    private Aventureiro aventureiro;
+    private Monstro monstro;
+
+    public Jogo(String nomeAventureiro, String nomeMonstro) {
+        this.aventureiro = new Aventureiro(nomeAventureiro);
+        this.monstro = new Monstro(nomeMonstro);
+    }
+}
